77afaa6f3a63f1144219f1f2e8890d02d15c6cde
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-04-18 23:00:53 -0300

    fix: remove unnecessary -b param

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
 # generate ssh key
-ssh-keygen -o -b 4096 -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C "myemail@mail.com"
+ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C "myemail@mail.com"
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
